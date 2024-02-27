78f20b19767ce26cf78c796155e58acadeead5d1
Ho_fan <yohoho0987612345@gmail.com>
2024-02-27 09:11:00 +0800

    Update byHabbits.hpp

--- src/modules/byHabbits.hpp
+++ src/modules/byHabbits.hpp
@@ -1,12 +1,17 @@
 #ifndef BYHABBITS_HPP
 #define BYHABBITS_HPP
 
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iterator>
 #include "matchmakingStrategy.hpp"
 
 
 class ByHabbits : MatchmakingStrategy
 {
 private:
+	int get_intersection_num(const std::vector<std::string>& matchmakerHabbits, const std::vector<std::string>& matchmakedHabbits);
 
 public:
 	std::shared_ptr<Individual> match(std::shared_ptr<Individual>& matchmaker, std::vector<std::shared_ptr<Individual>>& individuals, const bool& reverse) override;
