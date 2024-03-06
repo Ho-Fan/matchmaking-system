3efa572a8b12353a7969959ac9ce7c67e96594c4
Ho_fan <yohoho0987612345@gmail.com>
2024-03-06 09:14:50 +0800

    Update byHabbits.cpp

--- src/modules/byHabbits.cpp
+++ src/modules/byHabbits.cpp
@@ -12,5 +12,22 @@ int ByHabbits::get_intersection_num(const std::vector<std::string>& matchmakerHa
 
 std::shared_ptr<Individual> ByHabbits::match(std::shared_ptr<Individual>& matchmaker, std::vector<std::shared_ptr<Individual>>& individuals, const bool& reverse)
 {
-    return std::shared_ptr<Individual>();
+    std::shared_ptr<Individual> bestMatchmaked;
+    int maxHabbitNum = 0;
+    int minHabbitNum = std::numeric_limits<int>::max();
+
+    for (std::shared_ptr<Individual>& matchmaked : individuals) {
+        if (matchmaked == matchmaker) { continue; }
+
+        int tempHabbitNum = get_intersection_num(matchmaker->get_habbits(), matchmaked->get_habbits());
+        if (reverse == false && tempHabbitNum > maxHabbitNum) {
+            maxHabbitNum = tempHabbitNum;
+            bestMatchmaked = matchmaked;
+        }
+        else if (reverse == true && tempHabbitNum < minHabbitNum) {
+            minHabbitNum = tempHabbitNum;
+            bestMatchmaked = matchmaked;
+        }
+    }
+    return bestMatchmaked;
 }
